389191e16142bc0e24fcfdbf4b506fa86f8f9d73
James Cao <jcao5360@gmail.com>
2025-04-02 16:45:48 -0400

    init commit

---
+++ DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by James Cao on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
